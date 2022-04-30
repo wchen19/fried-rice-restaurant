3b6fcd937beed33f624f7d2d6a9d8bfc34f84037
Wilbert Chen <wilbertchen2001@gmail.com>
2022-04-30 21:43:55 +0700

    feat: complete navbar feature

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home/>} />
+        </Routes>
+      </Router>
+    </div>
     );
   }
   
