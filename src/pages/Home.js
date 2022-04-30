f43a37afa0f10af4ce73896a1061587e14a894f3
Wilbert Chen <wilbertchen2001@gmail.com>
2022-04-30 23:59:17 +0700

    feat: add home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import BannerImage from '../assets/scenery.jpg';
+import '../styles/Home.css';
 
 function Home() {
   return (
-    <div>Hello World</div>
+    <div className='home' style={{backgroundImage: `url(${BannerImage})`}}>
+      <div className='jumbotron'>
+        <h1>Wilbert's Restaurant</h1>
+        <p>React Restaurant Website Portfolio</p>
+        <Link to='menu'>
+          <button>Learn More</button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
